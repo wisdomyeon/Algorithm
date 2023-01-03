addd758bcd0db0a355fc39008fa5735691e1d836
wisdomyeon <duswl8936@naver.com>
2023-01-03 19:27:56 +0900

    문제 풀이 성공

---
+++ Programmers/Lv.0/삼각형의 완성조건(1).js
@@ -0,0 +1,12 @@
+function solution(sides) {
+    var answer = 0;
+    sides.sort((a, b) => {
+        return a - b;
+    })
+    if (sides[2] < sides[0] + sides[1]) {
+        answer = 1;
+    } else {
+        answer = 2;
+    }
+    return answer;
+}
